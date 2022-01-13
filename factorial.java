3d927a66871464ee380d9fc4acd24b49c4a8daa1
Nabatir <ritabanbanerjee2510@gmail.com>
2022-01-13 09:03:45 +0530

    Accepted number and displayed output through function

--- factorial.java
+++ factorial.java
@@ -13,5 +13,8 @@ class factorial
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        System.out.println(FindFactorial(num));
     }
 }
