e44922b47c79060c1353f2ffacda3cf8c835c13e
Nabatir <ritabanbanerjee2510@gmail.com>
2022-01-13 08:57:17 +0530

    Created functino FindFactorial

--- factorial.java
+++ factorial.java
@@ -1,5 +1,13 @@
 import java.util.*;
 class factorial
 {
-    
+    int FindFactorial(int n)
+    {
+        int fact=1;
+        for(int i=2;i<=N;i++)
+        {
+            fact*=i;
+        }
+    }
+    return fact;
 }
