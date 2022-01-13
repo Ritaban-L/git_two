869589be72d44492af9b1919da577fd270c0bfcb
Nabatir <ritabanbanerjee2510@gmail.com>
2022-01-13 08:59:44 +0530

    main class created with Scanner class object

--- factorial.java
+++ factorial.java
@@ -1,13 +1,17 @@
 import java.util.*;
 class factorial
 {
-    int FindFactorial(int n)
+    public static int FindFactorial(int n)
     {
         int fact=1;
         for(int i=2;i<=N;i++)
         {
             fact*=i;
         }
+         return fact;
     }
-    return fact;
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+    }
 }
